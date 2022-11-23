f53ca62b993cac83e9c23778085ede5a7dbb8ecc
Chaz Mateer <chaz.mateer@timmons.com>
2022-11-23 17:02:15 -0500

    Update import/export syntax per NG's request.

--- src/src/hooks/index.js
+++ src/src/hooks/index.js
@@ -1,3 +1,8 @@
-export { useConfig, useMapConfig, useLayout, useStaleData, useGet } from './useData';
-export { ProvideAuth, authContext, useAuth } from './useAuth';
-export * from './useIsModal';
+import { useConfig, useMapConfig, useLayout, useStaleData, useGet } from "./useData.js";
+import { ProvideAuth, authContext, useAuth } from "./useAuth.js";
+import useIsModal from "./useIsModal.js";
+export {
+  useConfig, useMapConfig, useLayout, useStaleData, useGet,
+  ProvideAuth, authContext, useAuth,
+  useIsModal,
+}
