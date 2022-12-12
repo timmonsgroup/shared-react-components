fbbdd72570bdfb543e493e3bf4545dcff19db054
Nathan Grant <nathan.grant@timmons.com>
2022-12-12 13:03:36 -0500

    Suppress error toast prop and custom formatters for toasts if needed

--- src/src/stories/GenericForm.jsx
+++ src/src/stories/GenericForm.jsx
@@ -19,9 +19,32 @@ import { useNavigate } from 'react-router-dom';
 import { useSnackbar } from 'notistack';
 import axios from 'axios';
 
+/**
+ * @param {object} props
+ * @param {string} props.formTitle - The title of the form
+ * @param {string} props.headerTitle - The title of the header
+ * @param {string} props.cancelUrl - The url to navigate to when the user cancels
+ * @param {string} props.successUrl - The url to navigate to when the user successfully submits the form
+ * @param {boolean} props.isEdit - Whether or not this is an edit form
+ * @param {object} props.defaultValues - The default values for the form
+ * @param {object} props.layoutOptions - The layout options for the form
+ * @param {boolean} props.twoColumn - Whether or not to render the form in two columns
+ * @param {string} props.domainUrl - The url for the domain
+ * @param {string} props.unitLabel - The label for the unit
+ * @param {string} props.helpText - The help text for the form
+ * @param {string} props.submitUrl - The url to submit the form to
+ * @param {function} props.formatPayload - A function to format the payload before submitting
+ * @param {function} props.onSuccess - A function to call when the form is successfully submitted
+ * @param {boolean} props.suppressSuccessToast - Whether or not to suppress the success toast
+ * @param {boolean} props.suppressErrorToast - Whether or not to suppress the success toast
+ * @param {function} props.formatSubmitMessage - A function to format the success toaster message
+ * @param {function} props.formatSubmitError - A function to format the error message (sends the error as a parameter and true if it came from the server)
+ * @returns
+ */
 const GenericForm = ({
     formTitle, headerTitle, cancelUrl, successUrl, isEdit, defaultValues, layoutOptions = {}, twoColumn = false,
-    domainUrl, unitLabel, helpText, submitUrl, formatPayload, onSuccess, suppressSuccessToast
+    domainUrl, unitLabel, helpText, submitUrl, formatPayload, onSuccess,
+    suppressSuccessToast, suppressErrorToast, formatSubmitMessage, formatSubmitError
   }) => {
   const [modifying, setModifying] = useState(false);
   const { sections, layoutLoading, control, reset, handleSubmit } = useDynamicForm(layoutOptions, defaultValues, domainUrl, setModifying);
@@ -40,7 +63,8 @@ const GenericForm = ({
       const result = await axios.post(submitUrl, orgData);
       if (result?.data?.streamID) {
         if (!suppressSuccessToast) {
-          enqueueSnackbar(`${unitLabel} successfully ${edit ? 'updated' : 'created'}`, { variant: 'success' });
+          const successMsg = formatSubmitMessage ? formatSubmitMessage(result) : `${unitLabel} successfully ${edit ? 'updated' : 'created'}`;
+          enqueueSnackbar(successMsg, { variant: 'success' });
         }
         // If we have an onSuccess callback, call it otherwise navigate to the successUrl
         if (onSuccess){
@@ -52,12 +76,20 @@ const GenericForm = ({
         }
       } else {
         setModifying(false);
-        enqueueSnackbar(`Error ${edit ? 'updating' : 'creating'} ${unitLabel}`, { variant: 'error' });
+        if(!suppressErrorToast) {
+          const errorMsg = formatSubmitError ? formatSubmitError(result) : `Error ${edit ? 'updating' : 'creating'} ${unitLabel}`;
+          enqueueSnackbar(errorMsg, { variant: 'error' });
+        }
       }
     } catch (error) {
-      // If we have a nice server error use it.
-      const serverError = error?.response?.data?.error;
-      enqueueSnackbar(serverError || `Error ${edit ? 'updating' : 'creating'} ${unitLabel}`, { variant: 'error' });
+      if(!suppressErrorToast) {
+        // If we have a nice server error use it.
+        const serverError = error?.response?.data?.error;
+        // Sending a true flag to formatSubmitError indicates that the error came from the server
+        const errorMsg = formatSubmitError ? formatSubmitError(error, true) : `Error ${edit ? 'updating' : 'creating'} ${unitLabel}`;
+
+        enqueueSnackbar(serverError || errorMsg, { variant: 'error' });
+      }
       setModifying(false);
     }
   };
