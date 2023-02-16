d55f44f97497e10e962619723601faf45504b9c6
Eric Schmiel <eric.schmiel@timmons.com>
2023-02-16 15:51:26 -0500

    Fixed package

--- src/.storybook/middleware.js
+++ src/.storybook/middleware.js
@@ -24,7 +24,7 @@ module.exports = function(app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://localhost:3001',
+            target: 'http://localhost:6006',
             changeOrigin: true,
             pathRewrite: {
               '^/api': ''
