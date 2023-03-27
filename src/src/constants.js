d84e1b53442d30863c505bb66ed5f236b9847571
Bryant Overgard <bryant.overgard@timmons.com>
2023-03-27 09:00:05 -0400

    Fixing oopsie

--- src/src/constants.js
+++ src/src/constants.js
@@ -60,7 +60,6 @@ export const MIN_LENGTH = 'minLength';
 export const EMAIL = 'email';
 export const ZIP = 'zip';
 export const PHONE = 'phone';
-export const DISABLED = 'disabled';
 
 export const CONDITIONAL_RENDER = Object.freeze({
   REQUIRED,
