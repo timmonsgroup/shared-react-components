b69c09785737535b5ea4e1c1414726cce1debee1
Nathan Grant <apeling@gmail.com>
2024-03-22 09:50:36 -0400

    Add some pre-release publish helpers

---
+++ timmons-group/publishBeta.sh
@@ -0,0 +1,12 @@
+# This will publish all the packages in the build folder to npm
+
+# Exit on error
+set -e
+
+# Publish all packages in the build folder
+cd build
+for dir in */; do
+  cd $dir
+  npm publish --tag beta
+  cd ..
+done
