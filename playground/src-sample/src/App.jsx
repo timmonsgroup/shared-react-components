f99a2f97da933bc645ba29e5b853245a65ba9a7f
Bryant Overgard <bryant.overgard@timmons.com>
2024-04-09 16:06:10 -0400

    Playground is able to use mui components

--- playground/src-sample/src/App.jsx
+++ playground/src-sample/src/App.jsx
@@ -4,12 +4,16 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 import { LineLoader } from '@timmons-group/shared-react-components/components'
+import { LoadingSpinner } from '@timmons-group/shared-react-components/components'
+import Theme from '@timmons-group/shared-react-components/muiTheme'
+
+import { ThemeProvider } from '@mui/material/styles'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <ThemeProvider theme={Theme}>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -21,6 +25,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <LineLoader />
+        <LoadingSpinner isActive={true} />
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -31,7 +36,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </ThemeProvider>
   )
 }
 
