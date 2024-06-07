a401a018a4f9fff62bb2f6da00645911c97a96b6
Nathan Grant <nathan.grant@timmons.com>
2024-06-07 10:12:57 -0400

    Build fixes and config form test

---
+++ playground/src-sample/src/ConfigFormTester.jsx
@@ -0,0 +1,43 @@
+import { ConfigForm, GenericConfigForm, defaultChoiceMapper } from "@timmons-group/shared-react-components";
+import { layout } from "./helpers/FormLayoutPreset";
+
+export const choiceFormatter = (fieldId, response, options) => {
+  if (fieldId === '') {
+    const { data } = response || {};
+    const formattedChoices = data.map((choiceDataItem, index) => {
+      return { id: index, label: choiceDataItem.name };
+    });
+
+    console.log('using Custom for fieldId')
+    return formattedChoices;
+  }
+
+  console.log('using SRC default')
+  console.log('choiceFormatter', fieldId, response, options);
+  return defaultChoiceMapper(response, options);
+}
+
+const ConfigFormTester = () => {
+  const formData = {
+    email: 'duderino@gmail.com'
+  }
+
+  const onSubmit = (data) => {
+    console.log('onSubmit', data)
+  }
+
+
+
+  return (
+    <ConfigForm formLayout={layout.layout} data={formData} parseOptions={{ choiceFormatter }}>
+      <GenericConfigForm
+        headerTitle="Bacon Bits"
+        // isEdit={isEdit}
+        // sectionProps={sectionProps}
+        onSubmit={onSubmit}
+      />
+    </ConfigForm>
+  )
+}
+
+export default ConfigFormTester
