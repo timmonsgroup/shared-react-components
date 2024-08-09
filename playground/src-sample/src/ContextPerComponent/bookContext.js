27a78617d36c5b275c37bc2671ea262a54fbe6ad
Nathan Grant <nathan.grant@timmons.com>
2024-08-09 10:32:48 -0400

    Bad copy pasta

--- playground/src-sample/src/ContextPerComponent/bookContext.js
+++ playground/src-sample/src/ContextPerComponent/bookContext.js
@@ -6,7 +6,7 @@ import {
 export const BookContext = createContext(null);
 
 export const useBookContext = () => {
-  // The context object will contain anything returned by useProvideAuth
+  // The context object will contain anything returned by BookProvider value prop
   const context = useContext(BookContext);
   if (context === undefined) {
     throw new Error('useBookContext must be used within a BookProvider');
